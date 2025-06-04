fdeac87771680fe6aee7f9e78f557fba6bd6394e
Rick Console <rick@rickconsole.com>
2025-06-04 15:15:13 -0400

    updated for newer raspbian versions

--- install.sh
+++ install.sh
@@ -46,6 +46,7 @@ echo -e "\e[0;31m#\e[0m                                                         
 echo -e "\e[0;31m#############################################################\e[0m"
 read -e NULL
 
+INITIAL_DIR=$(pwd)
 echo "[*] Starting installation."
 
 #Checking if root. Will sudo
@@ -104,8 +105,12 @@ echo "[*] Proceeding with installation."
 echo "[*] Updating raspbian. This could take a while..."
 apt-get update
 apt-get -y dist-upgrade
-apt-get -y install apache2 hostapd isc-dhcp-server screen ntpdate
+apt-get -y install apache2 hostapd dnsmasq dhcpcd screen ntpdate
 echo "[*] Updating complete."
+
+echo "[*] Stopping services for configuration..."
+sudo systemctl stop hostapd
+sudo systemctl stop dnsmasq
 
 #Changing hostname for purely vain reasons
 echo "[*] Setting Wiegotcha hostname"
@@ -114,26 +119,50 @@ echo 'Wiegotcha' > /etc/hostname
 
 #Insatlling WiringPi
 echo "[*] Installing WiringPi."
-git clone git://git.drogon.net/wiringPi 
-cd wiringPi
+git clone https://github.com/WiringPi/WiringPi.git 
+cd WiringPi
 ./build
 cd ~/
 
 #Copying Wiegotcha specific conf files
-#Configs include: dhcpd, hostapd, interfaces, rc.local, and default html pages
+#Configs include: hostapd, dnsmasq, dhcpcd, and default html pages
 #Probably need to mod /etc/modules
 echo "[*] Copying config files"
-cd ./Wiegotcha/
-cp ./confs/dhcpd.conf /etc/dhcp/
-cp ./confs/hostapd.conf /etc/hostapd/
-cp ./confs/interfaces /etc/network/
-cp -R ./html/* /var/www/html/
-cp ./confs/rctmp.local /etc/rc.local
-cp ./laststep.sh ../
-mkdir /var/www/html/backup/
+
+echo "[*] Configuring static IP for wlan0 in /etc/dhcpcd.conf..."
+sudo cp "$INITIAL_DIR/confs/dhcpcd.conf" /etc/dhcpcd.conf
+
+echo "[*] Restarting dhcpcd service..."
+sudo systemctl restart dhcpcd
+
+echo "[*] Configuring dnsmasq..."
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig 2>/dev/null || true
+sudo cp "$INITIAL_DIR/confs/dnsmasq.conf" /etc/dnsmasq.conf
+
+# Note: Removed 'cd ./Wiegotcha/' as paths are now absolute using $INITIAL_DIR
+sudo cp "$INITIAL_DIR/confs/hostapd.conf" /etc/hostapd/
+sudo cp -R "$INITIAL_DIR/html/"* /var/www/html/
+sudo cp "$INITIAL_DIR/laststep.sh" ./laststep.sh # Copies to /root/laststep.sh
+sudo mkdir -p /var/www/html/backup/ # Added -p to mkdir
+
+echo "[*] Setting up Wiegotcha systemd service..."
+sudo cp "$INITIAL_DIR/confs/wiegotcha.service" /etc/systemd/system/
+sudo systemctl daemon-reload
 sed -i 's|#DAEMON_CONF=""|DAEMON_CONF=/etc/hostapd/hostapd.conf|g' /etc/default/hostapd
+
+echo "[*] Finalizing service configuration..."
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl enable dnsmasq
+sudo systemctl enable dhcpcd
+sudo systemctl enable wiegotcha.service
+echo "[*] Starting services..."
+sudo systemctl start dhcpcd
+sudo systemctl start dnsmasq
+sudo systemctl start hostapd
+
 echo "[*] Compiling Wiegotcha C code"
-gcc -o ../wiegotcha wiegotcha.c -L/usr/local/lib -lwiringPi -lpthread
+sudo gcc -o ./wiegotcha "$INITIAL_DIR/wiegotcha.c" -L/usr/local/lib -lwiringPi -lpthread # Compiles to /root/wiegotcha
 
 #Enable i2c on boot for hardware clock
 #TODO: This is currently done via raspi-config.
